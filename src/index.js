caf05b8cd920524a5f2184665ec21f225716bc7b
DzmitryK89 <43322279+DzmitryK89@users.noreply.github.com>
2022-03-11 23:09:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,9 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let emptyArr = []
+    for (let i = 0; i < expr.length; i = i + 10) {
+        emptyArr.push(expr.slice(i, i + 10))
+    }
+    let mappedEmptyArr = emptyArr.map(x => x.length < 10 ? x.padStart(10, '0') : x) 
+    let withSpaceArr = mappedEmptyArr.map(s => s === '**********' ? ' ': s) 
+    let replaceArr = withSpaceArr.map(y => y.replace(/10/g, '.'))
+    let replace1Arr = replaceArr.map(z => z.replace(/11/g, '-'))
+    let withoutNullArr = replace1Arr.map(k => k.replace(/00/g, '')) 
+    
+    let result = withoutNullArr.map(a => a === ' ' ? ' ':MORSE_TABLE[a])
+    let finalResult = ''
+    for (let i = 0; i < result.length; i++) {
 
+        finalResult = finalResult + result[i]   
+    }
+    return finalResult
+}
 module.exports = {
     decode
 }
